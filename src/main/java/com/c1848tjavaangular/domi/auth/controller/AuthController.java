7ef42983ba7995652b095e3b9175b6d921421c30
Jesús Valeta <jvaleta1995@gmail.com>
2024-08-09 14:02:46 -0500

    Documentacion de los endpoints

--- src/main/java/com/c1848tjavaangular/domi/auth/controller/AuthController.java
+++ src/main/java/com/c1848tjavaangular/domi/auth/controller/AuthController.java
@@ -5,6 +5,7 @@ import com.c1848tjavaangular.domi.auth.dto.LoginDto;
 import com.c1848tjavaangular.domi.auth.dto.RegisterDto;
 import com.c1848tjavaangular.domi.auth.service.AuthService;
 import com.c1848tjavaangular.domi.models.entities.Usuarios;
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -22,12 +23,14 @@ public class AuthController {
         this.authService = authService;
     }
 
+    @Operation(summary = "Permite al usuario loguearse y obtener el token de autenticacion")
     @PostMapping("/login")
     public ResponseEntity<JwtResponseDto> login(@RequestBody LoginDto loginDto) {
 
             return ResponseEntity.ok(authService.login(loginDto));
     }
 
+    @Operation(summary = "Permite crear cuenta al usuario")
     @PostMapping("/registro")
     public ResponseEntity<Usuarios> register(@RequestBody RegisterDto registerDto) {
 
--- src/main/java/com/c1848tjavaangular/domi/controllers/UsuariosController.java
+++ src/main/java/com/c1848tjavaangular/domi/controllers/UsuariosController.java
@@ -5,6 +5,8 @@ import com.c1848tjavaangular.domi.dtos.PasswordDto;
 import com.c1848tjavaangular.domi.dtos.UsuariosDto;
 import com.c1848tjavaangular.domi.services.UsuariosService;
 import com.c1848tjavaangular.domi.services.impl.UploadFileService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -28,6 +30,7 @@ public class UsuariosController {
         this.jwtService = jwtService;
         this.uploadFileService = uploadFileService;
     }
+    @Operation(summary = "Permite actualizar perfil del usuario", security = @SecurityRequirement(name = "bearerAuth"))
     @PutMapping(value = "/usuario",consumes = "multipart/form-data")
     public ResponseEntity<?> updatePerfil(@RequestPart("foto") MultipartFile foto, @RequestPart("portada") MultipartFile portada,@RequestHeader("Authorization") String token, @RequestPart UsuariosDto usuariosDto) throws IOException {
         String nombreFoto = uploadFileService.saveFoto(foto);
@@ -38,6 +41,7 @@ public class UsuariosController {
         return ResponseEntity.ok().body(usuariosService.updatePerfil(idUsuario, usuariosDto));
     }
 
+    @Operation(summary = "Permite actualizar password", security = @SecurityRequirement(name = "bearerAuth"))
     @PutMapping("/usuario/password")
     public ResponseEntity<?> updatePassword(@RequestHeader("Authorization") String token, @RequestBody PasswordDto passworDto) {
         Integer idUsuario = jwtService.getIdUsuarioFromToken(token);
@@ -45,6 +49,7 @@ public class UsuariosController {
         return ResponseEntity.ok().body("Contraseña actualizada!");
     }
 
+    @Operation(summary = "Permite obtener perfil del usuario", security = @SecurityRequirement(name = "bearerAuth"))
     @GetMapping("/usuario/perfil")
     public ResponseEntity<?> getUsuario(@RequestHeader("Authorization") String token){
         Integer idUsuario = jwtService.getIdUsuarioFromToken(token);
